60c95118d59324a5312b7f45cc94ccfdb6582069
Jan Winkler <jan.winkler.84@gmail.com>
2016-08-19 16:09:13 +0200

    First version of ATtiny85 payload

---
+++ ATtiny85/Serial_RGB/Serial_RGB.ino
@@ -0,0 +1,70 @@
+#include <SoftSerialHacked.h>
+#include <Adafruit_NeoPixel.h>
+
+
+#ifdef __AVR__ 
+  #include <avr/power.h>
+#endif
+
+
+// Defines
+#define LEDS_PIN       1
+#define LEDS_PER_RING  12
+#define RINGS          5
+#define NUM_LEDS       (LEDS_PER_RING * RINGS)
+#define RX_PIN         3
+#define BUFFER_SIZE    64
+
+
+// Global variables
+Adafruit_NeoPixel npPixels = Adafruit_NeoPixel(NUM_LEDS, LEDS_PIN, NEO_GRB + NEO_KHZ800);
+SoftSerialHacked softSerial(RX_PIN);
+
+
+char cBuffer[BUFFER_SIZE];
+unsigned int unBufferIndex = 0;
+
+unsigned int unActiveRing = 0;
+
+
+void setup() {
+  npPixels.begin();
+  
+  pinMode(RX_PIN, INPUT);
+  softSerial.begin(9600);
+}
+
+
+void loop() {
+  if(softSerial.available()) {
+    char c = softSerial.read();
+    
+    if(c == 13) {
+      int nInteger, nR, nG, nB;
+      
+      if(sscanf(cBuffer, "brightness(%d)", &nInteger) != EOF) {
+        npPixels.setBrightness(nInteger);
+      } else if(sscanf(cBuffer, "ring(%d)", &nInteger) != EOF) {
+        unActiveRing = nInteger;
+      } else if(sscanf(cBuffer, "led(%d, %d, %d, %d)", &nInteger, &nR, &nG, &nB) != EOF) {
+        npPixels.setPixelColor(unActiveRing * 12 + nInteger, npPixels.Color(nR, nG, nB));
+      } else if(sscanf(cBuffer, "show()") != EOF) {
+        npPixels.show();
+      } else if(sscanf(cBuffer, "clear()") != EOF) {
+        for(unsigned int unI = 0; unI < NUM_LEDS; ++unI) {
+          npPixels.setPixelColor(unI, npPixels.Color(0, 0, 0));
+        }
+        
+        npPixels.show();
+      }
+      
+      unBufferIndex = 0;
+    } else {
+      if(!(unBufferIndex >= BUFFER_SIZE - 1)) {
+        cBuffer[unBufferIndex] = c;
+        unBufferIndex++;
+      }
+    }
+  }
+}
+
